1cf109c41a6f968a703839a6bbd250f3b781bf73
Yuliia Synytsia <lm.julia92@gmail.com>
2015-09-10 22:04:10 -0700

    Fix problem with exceptions. Backwash not fixed.

--- PercolationStats.java
+++ PercolationStats.java
@@ -7,9 +7,9 @@ import edu.princeton.cs.algs4.StdOut;
 public class PercolationStats {
     private int row;
     private int column; 
-    private boolean[][] gridTwoD; 
+    private int[][] gridTwoD; 
     private Percolation percolationForStats;
-    private int openedSites;
+    private double openedSites;
     private double[] percolationTreshold;
     private int numberOfExperiment;
     public PercolationStats(int N, int T){     // perform T independent experiments on an N-by-N grid
@@ -17,19 +17,23 @@ public class PercolationStats {
             throw 
                 new IllegalArgumentException("N, T must be bigger than 0.");
         }
-         gridTwoD = new boolean[N][N];
-         percolationForStats = new Percolation(N);
+         
          percolationTreshold = new double[T];
          numberOfExperiment = T;
          
         for(int i = 0; i < T; i++){
+            gridTwoD = new int[N][N];
+            percolationForStats = new Percolation(N);
+            openedSites = 0.0;
              for(int j = 0; j < N*N; j++ ){
                  row = StdRandom.uniform(N) + 1;
                  column = StdRandom.uniform(N) + 1;
-                 if(gridTwoD[row][column] == true) continue;
+                 if(gridTwoD[row-1][column-1] == 1) {
+                     continue;
+                 }
                  else{
-                     gridTwoD[row][column] = true;
-                     percolationForStats.open(row + 1, column + 1);
+                     gridTwoD[row-1][column-1] = 1;
+                     percolationForStats.open(row, column);
                      openedSites++;
                      if(percolationForStats.percolates()){
                          percolationTreshold[i] = openedSites/(N*N);
@@ -59,9 +63,12 @@ public class PercolationStats {
     }
 
     public static void main(String[] args){    // test client (described below)
-         PercolationStats pstats = 
-            new PercolationStats(Integer.parseInt(args[0]), 
-                Integer.parseInt(args[1]));
+        PercolationStats pstats; 
+        if (args.length == 2){
+            pstats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        }
+        else 
+        pstats = new PercolationStats(200, 100);
         StdOut.println("mean\t\t\t = " + pstats.mean());
         StdOut.println("stddev\t\t\t = " + pstats.stddev());
         StdOut.println("95% confidence interval\t = " + pstats.confidenceLo()
